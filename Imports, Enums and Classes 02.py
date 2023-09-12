fcc7544b6f559d2783360cbe4d29b42ec5d1a462
yoda-gell <hvuskv6@gmail.com>
2023-09-12 10:10:26 +0300

    finish pdf 2

---
+++ Imports, Enums and Classes 02.py
@@ -0,0 +1,37 @@
+#1. we use import to bring code in module available to another
+
+#2. you give the fath C:\Users\YODA\OneDrive\...\..
+
+#3. you can control which code is executed if is true you executed if is false you skipped 
+
+#4. class is like a object constructor 
+
+#5 to build object 1 is more organized 2 you can control you build that 3 and to use polymorphism
+
+#6 attributes are variables of a class
+
+#7 variables that contain data and functions that can be used to manipulate the data
+
+#8 constructor is to assign values Destructor is delete the object from the memory is auto 
+
+#9 is word that you cannot be use as var name like  and ,or, break ... 
+# and self if reference to the current instate of the class and use to access var in this class 
+
+#10 inherits all the methods from another class
+
+#11 methods/functions/operators with the same name that can be executed on many objects or classes
+# when we have multiple classes with the same method 
+
+#12 static method is bound to a class rather than the objects this means that a static method can
+#be called without object      @static_method def....
+
+from Person import Person
+def main():
+    for i in range(10):
+        p1 = Person()
+        print(p1)
+if __name__ == "__main__":
+    main()  
+
+
+#13 enum is class  enum can be compared by symbolic to by more read and safe
