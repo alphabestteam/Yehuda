d9dba95c90f35cb3a7e05ffe7ae23bcfd7054eeb
yoda-gell <hvuskv6@gmail.com>
2023-09-12 13:31:44 +0300

    formating

--- supermarket2.0.py
+++ supermarket2.0.py
@@ -3,9 +3,9 @@ from Product import Product
 from Costumer import Costumer
 from register import Register
 
-p = Product("milk",2,2)
-m = Product("milk",5,6)
-e = Product("emilk",1,5)
+p = Product("milk", 2, 2)
+m = Product("milk", 5, 6)
+e = Product("emilk", 1, 5)
 c = Costumer("yehuda")
 r = Register()
 
@@ -19,11 +19,10 @@ r.checkout_customer(c)
 r.print_summary()
 print((c.shopping_list))
 print(c.receipt)
-dict_json ={}
+dict_json = {}
 for i in r._total_sales:
     cart = [p for p in c.shopping_list]
-    dict_json[i] = {"customer:" :i , "cart :": cart  , "total :" :r.profit_amount}
-    
+    dict_json[i] = {"customer:": i, "cart :": cart, "total :": r.profit_amount}
+
 with open("items.json", "w") as outfile:
     json.dump(dict_json, outfile)
-
