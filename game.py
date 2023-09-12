7ebdf10392bae2ef59927021da28821ffa9e50de
yoda-gell <hvuskv6@gmail.com>
2023-09-12 13:32:49 +0300

    format

--- game.py
+++ game.py
@@ -1,8 +1,13 @@
 from monster import Monster
 from hero import Hero
 
-def choose_action(monster:Monster,hero:Hero):
-    action =int(input("enter 1 - for attack ,enter 2 - for level up , enter 3- for heal, enter 4 - for defend    "))
+
+def choose_action(monster: Monster, hero: Hero):
+    action = int(
+        input(
+            "enter 1 - for attack ,enter 2 - for level up , enter 3- for heal, enter 4 - for defend    "
+        )
+    )
     if action == 1:
         hero.increase_coins(1)
         hero.attack(monster)
@@ -17,21 +22,20 @@ def choose_action(monster:Monster,hero:Hero):
         hero.increase_coins(1)
     else:
         print("pls enter 1 or 2 or 3 or 4")
-    
+
 
 def main():
     h = Hero("yehuda")
-    m = Monster("you",h.level)
-    while h.hp !=0:
-        choose_action(m,h)
-        if m.hp ==0:
-            m = Monster("you",h.level)
-        if m.hp>1:
+    m = Monster("you", h.level)
+    while h.hp != 0:
+        choose_action(m, h)
+        if m.hp == 0:
+            m = Monster("you", h.level)
+        if m.hp > 1:
             m.attack(h)
-        if h.hp ==0:
+        if h.hp == 0:
             print("you loser go back to mommy")
 
-    
 
 if __name__ == "__main__":
     main()
