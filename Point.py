38c027fb0265fbc64c841d5d20a1ad1b85d1f2e5
yoda-gell <hvuskv6@gmail.com>
2023-09-12 14:31:36 +0300

    without magic function

---
+++ Point.py
@@ -0,0 +1,6 @@
+class Point:
+    def __init__(self,x,y) -> None:
+        self._x =x 
+        self._y =y
+        
+    
---
+++ oop-special functions.py
@@ -0,0 +1,22 @@
+from Point import Point
+
+# 1. magic functions are __some__that defining objects to be sub plus str pow ect..'__abs__', '__add__', '__and__', '__bool__', 
+# 2. it will print to as 0xsdjsdfjsdf and not the string that we wont
+# 3. the same he compere the 0xsjdfkdsf and not the string 
+# 4. you can use + in int and in str how ?  because you overloaded by int class and str 
+# 5.
+def without_magic_functions():
+    p1 = Point(5,6)
+    p2 = Point(5,6)
+    
+    print(p1==p2)
+    print(p1,p2)
+    print(p1+p2)
+    
+#the output is False and Point.Point object at 0x00000227029A4FD0> <Point.Point object at 0x00000227029A4F70>
+# and error because cant + two Point
+
+ 
+
+if __name__=="__main__":
+    without_magic_functions()
