539fddf122933521c0fbb27261d59a084a2d7b4f
yoda-gell <hvuskv6@gmail.com>
2023-09-14 10:03:29 +0300

    finish pdf 4

--- client.py
+++ client.py
@@ -2,22 +2,22 @@ import socket
 
 
 def client_program():
-    host = socket.gethostname()  
-    port = 5000  
+    host = socket.gethostname()
+    port = 5000
 
-    client_socket = socket.socket()  
-    client_socket.connect((host, port))  
+    client_socket = socket.socket()
+    client_socket.connect((host, port))
 
-    message = input(" -> ")  
-    
-    client_socket.send(message.encode()) 
-    data = client_socket.recv(1024).decode() 
+    message = input(" -> ")
 
-    print('Received from server: ' + data)  
+    client_socket.send(message.encode())
+    data = client_socket.recv(1024).decode()
 
-    input("enter for exits ")  
+    print('Received from server: ' + data)
 
-    client_socket.close()  
+    input("enter for exits ")
+
+    client_socket.close()
 
 
 if __name__ == '__main__':
--- server.py
+++ server.py
@@ -4,23 +4,23 @@ import socket
 def server_program():
 
     host = socket.gethostname()
-    port = 5000  
+    port = 5000
 
-    server_socket = socket.socket()  
-    server_socket.bind((host, port)) 
+    server_socket = socket.socket()
+    server_socket.bind((host, port))
 
     server_socket.listen(2)
-    
-    conn, address = server_socket.accept() 
+
+    conn, address = server_socket.accept()
     print("Connection from: " + str(address))
 
     data = conn.recv(1024).decode()
-  
+
     print("from connected user: " + str(data))
 
-    conn.send(data.encode().upper())  
+    conn.send(data.encode().upper())
 
-    conn.close()  
+    conn.close()
 
 
 if __name__ == '__main__':
