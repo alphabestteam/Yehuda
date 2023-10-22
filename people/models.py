f98fed29d355800030d9e3da6a24487c02489768
yoda-gell <hvuskv6@gmail.com>
2023-10-22 15:37:43 +0300

    almost finish

--- people/models.py
+++ people/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime
 
 class Person(models.Model):
     name = models.CharField(max_length=50)
@@ -6,6 +7,10 @@ class Person(models.Model):
     date_of_birth =models.DateField()
     hometown = models.CharField(max_length=50)
 # Create your models here.
+    def under_18(self):
+        age_now = datetime.now().year - 18
+        return self.date_of_birth.year <= age_now
+            
 class Parent(Person):
     work_space = models.CharField(max_length=50)
     salary = models.IntegerField()
