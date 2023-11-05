44af4676335cd0df9690996c133458e2446ec4b3
yoda-gell <hvuskv6@gmail.com>
2023-11-05 18:32:43 +0200

    finish pdf 17

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 const main = document.querySelector('main');
 
-
-
 // const naiveHead1 = createHeading('red', 'naive 1');
 // const naiveHead2 = createHeading('blue', 'naive 2');
 
@@ -9,21 +7,29 @@ const main = document.querySelector('main');
 // main.appendChild(naiveHead1);
 // main.appendChild(naiveHead2);
 
-function createHeading(color, text){
+// function createHeading(color, text){
+//     const heading = document.createElement('h1');
+//     heading.setAttribute('style', 'color: ' + color);
+//     heading.textContent = text;
+//     return heading;
+// }
+
+// let count = 1
+function headingFactory(color){
     const heading = document.createElement('h1');
     heading.setAttribute('style', 'color: ' + color);
+    function createHeading(text){
     heading.textContent = text;
-    return heading;
+    main.appendChild(heading)
+    }
+    return createHeading
 }
+const createHeaderRed = headingFactory("red")
+const createHeaderBlue = headingFactory("blue")
 
-let count = 1
-function headingFactory(color){
-    factory = createHeading(color,"using factory " + count)
-    count ++ ;
-    main.appendChild(factory)
-}
+createHeaderRed("using factory 1")
+createHeaderBlue("using factory 2")
 
-const trd = headingFactory("red")
-headingFactory("blue")
+
 //implement the closure here and use it. 
-    //keep in mind you return a function. think about how one should use what this function will return.
+//keep in mind you return a function. think about how one should use what this function will return.
